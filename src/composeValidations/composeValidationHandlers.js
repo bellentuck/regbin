c2ca5dac875fd6e17a9f062b3a6de81b3c1315e0
Ben Ellentuck <ellentuckben@gmail.com>
2018-04-29 14:05:35 -0400

    custom message-adding works

--- src/composeValidations/composeValidationHandlers.js
+++ src/composeValidations/composeValidationHandlers.js
@@ -1,6 +1,6 @@
 /*
   Input:
-    E.g.,  ('name', 'a-z', 'required') or ('address', 'city') or ('a-z', {range: [4, 12]})
+    E.g.,  ('name', 'a-z', 'required') or ('address', 'city') or ('a-z', {range: [4, 12]}) or ('name', [value => /walrus/i.test(value), 'must be a walrus'], 'required')
 
   Output: Map of entries like { rule1 => handler1, rule2 => handler2, ... }
   where each "handler" is a validation handler mapping of validation function to message-generating functions for that rule.
@@ -11,8 +11,9 @@ module.exports = (...rules) => {
   return rules.map(rule => {
     if (typeof rule === 'string' && validations.hasOwnProperty(rule)) {
       return validations[rule];
-    } else if (Array.isArray(rule)) { // custom validations
-      return rule;   // will already be in form [fn, message].
+    } else if (Array.isArray(rule)) { // Custom validations: [fn, message].
+      if (typeof rule[1] === 'function') return rule;
+      else return [ rule[0], () => rule[1] ];
     } else {  // rule is an object, like {range: [4,12]} or {areaCode: true}
       const ruleName = Object.keys(rule)[0];
       const ruleSpecs = rule[ruleName];
